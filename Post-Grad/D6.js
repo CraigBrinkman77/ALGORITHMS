ab177d88c4e1ba7f0afe47fc355924c58db913fd
Craig Brinkman <craigfrancis77@gmail.com>
2022-03-21 12:17:44 -0600

    algo push

---
+++ Post-Grad/D6.js
@@ -0,0 +1,32 @@
+let nums = [1, 2, 3, 1]
+// output : 2
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findPeakElement = function (nums) {
+    if (nums.length == 1) {
+        return 0;
+    }
+    if (nums.length == 2) {
+        if (nums[0] < nums[1]) {
+            return 1;
+        } else return 0;
+    }
+    let n = nums.length - 1;
+    for (let i = 0; i <= n; i++) {
+        if (i == 0 && nums[i] > nums[i + 1]) {
+            return i;
+        }
+        else if (i == n && nums[i] > nums[i - 1]) {
+            return i;
+        }
+        else if (i != 0 && i != n) {
+            if (nums[i] > nums[i + 1] && nums[i] > nums[i - 1]) {
+                return i;
+            }
+        }
+    }
+};
+console.log(findPeakElement(nums));
